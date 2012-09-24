b8d7200fbc3a0d1f900074c169f157d984a17bf2
Geoff Simmons <geoff@uplex.de>
2012-09-24 15:23:02 +0200

    trackrdrd: More error-checking in the build-ci script (Jenkins builds)

--- trackrdrd/build-ci.sh
+++ trackrdrd/build-ci.sh
@@ -9,20 +9,25 @@ cd $WORKSPACE/lhotse-varnish-cache
 git pull origin 3.0_bestats | head -1 | grep 'Already up-to-date'
 if [ $? -eq 1 ]; then
     ./autogen.sh
+    [[ $? -ne 0 ]] && exit 1
     CFLAGS=-m64 ./configure
+    [[ $? -ne 0 ]] && exit 1
     make
+    [[ $? -ne 0 ]] && exit 1
 fi
 
 cd $WORKSPACE/trackrdrd
 ./autogen.sh
+[[ $? -ne 0 ]] && exit 1
+
 CFLAGS=-m64 ./configure VARNISHSRC=$WORKSPACE/lhotse-varnish-cache
+[[ $? -ne 0 ]] && exit 1
+
 make clean
 make
-[[ $? -ne 0 ]] && GLOBAL_STATUS=1
+[[ $? -ne 0 ]] && exit 1
 
 make check
-[[ $? -ne 0 ]] && GLOBAL_STATUS=1
+[[ $? -ne 0 ]] && exit 1
 
 make clean
-
-exit $GLOBAL_STATUS
