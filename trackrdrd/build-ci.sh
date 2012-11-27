f8b0e938366f24882a976c88e3c90f2f5b580859
Geoff Simmons <geoff@uplex.de>
2012-11-27 09:54:23 +0100

    trackrdrd: initial rpm build, added to CI build script

--- trackrdrd/build-ci.sh
+++ trackrdrd/build-ci.sh
@@ -1,6 +1,8 @@
 #/bin/sh
 
 GLOBAL_STATUS=0
+LHOTSE_VARNISH_PREFIX=/var/opt/varnish
+LHOTSE_TRACKING_PREFIX=/var/opt/varnish_tracking
 
 if [ ! -d "$WORKSPACE/lhotse-varnish-cache" ]; then
   git clone git@git.lhotse.ov.otto.de:lhotse-varnish-cache
@@ -20,7 +22,7 @@ cd $WORKSPACE/trackrdrd
 ./autogen.sh
 [[ $? -ne 0 ]] && exit 1
 
-CFLAGS=-m64 CXXFLAGS=-m64 LDFLAGS=-Wl,-rpath=/var/opt/varnish/lib/varnish:/var/opt/varnish/lib ./configure --prefix=/var/opt/varnish_tracking VARNISHSRC=$WORKSPACE/lhotse-varnish-cache
+CFLAGS=-m64 CXXFLAGS=-m64 LDFLAGS=-Wl,-rpath=$LHOTSE_VARNISH_PREFIX/lib/varnish:$LHOTSE_VARNISH_PREFIX/lib ./configure --prefix=$LHOTSE_TRACKING_PREFIX VARNISHSRC=$WORKSPACE/lhotse-varnish-cache
 [[ $? -ne 0 ]] && exit 1
 
 make clean
@@ -30,4 +32,18 @@ make
 make check
 [[ $? -ne 0 ]] && exit 1
 
-make clean
+VERSION=$($WORKSPACE/trackrdrd/src/.libs/trackrdrd -V | awk '{print $2}')
+REVISION=$($WORKSPACE/trackrdrd/src/.libs/trackrdrd -V | awk '{print $4}')
+BUILDPATH=$WORKSPACE/trackrdrd/rpmbuild/BUILDROOT/trackrdrd-$VERSION-rev${REVISION}_build$JENKINS_BUILD_NUMBER.$(uname -m)
+DESTDIR=$BUILDPATH make install
+[[ $? -ne 0 ]] && exit 1
+
+mkdir -p $BUILDPATH/etc/init.d
+mkdir $BUILDPATH/$LHOTSE_TRACKING_PREFIX/etc
+cp $WORKSPACE/trackrdrd/etc/sample.conf $BUILDPATH/etc/trackrdrd.conf
+cp $WORKSPACE/trackrdrd/etc/sample.conf $BUILDPATH/$LHOTSE_TRACKING_PREFIX/etc/
+cp $WORKSPACE/trackrdrd/etc/trackrdrd $BUILDPATH/etc/init.d/
+
+cd $WORKSPACE/trackrdrd/rpmbuild
+rpmbuild --define '_topdir '$WORKSPACE/trackrdrd/rpmbuild --define 'version '$VERSION --define 'revision '$REVISION --define 'build_number '$JENKINS_BUILD_NUMBER --define 'prefix '$LHOTSE_TRACKING_PREFIX -bb SPECS/trackrdrd.spec
+[[ $? -ne 0 ]] && exit 1
