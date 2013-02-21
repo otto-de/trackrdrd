d6a7ba553b2105acda5b4caf7f8489470314f64a
Geoff Simmons <geoff@uplex.de>
2013-02-21 21:41:12 +0100

    trackrdrd: reworked SPMCQ, waits for a full queue are never necessary

--- src/test/test_spmcq_loop.sh
+++ src/test/test_spmcq_loop.sh
@@ -9,10 +9,10 @@ echo "... running test_spmcq $N times"
 I=1
 while [[ $I -le $N ]]
 do
-    # echo -en "Test $N\r"
-    ./test_spmcq > /dev/null
+    MSG=$(./test_spmcq) 
     if [ $? -ne 0 ]; then
         echo "Test $I FAILED"
+	echo $MSG
         exit 1
     fi
     ((I++))
