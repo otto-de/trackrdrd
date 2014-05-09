6f08c55fee517631e55d7c106f952d8c0b1988e5
Geoff Simmons <geoff@uplex.de>
2014-05-09 12:16:26 +0200

    added a .dir-locals file to help with code style in emacs

---
+++ trackrdrd/.dir-locals.el
@@ -0,0 +1,9 @@
+;; Code style for this project:
+;; * Indentation with 4 spaces (tabs are evil)
+;; * 80 column limit
+;; * otherwise BSD C style
+(((nil . ((indent-tabs-mode . f)
+          (tab-width . 4)
+          (fill-column . 80))))
+ (c-mode . ((c-file-style . "BSD")
+            (c-basic-offset . 4))))
