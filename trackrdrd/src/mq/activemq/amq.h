7be3e248e6c703d4bb74f752f145fb15ef02c26e
Geoff Simmons <geoff@uplex.de>
2014-05-16 15:47:33 +0200

    trackrdrd ActiveMQ MQ implementation: bugfix MQ_Reconnect by managing worker and connection objects correctly

--- trackrdrd/src/mq/activemq/amq.h
+++ trackrdrd/src/mq/activemq/amq.h
@@ -52,13 +52,15 @@ private:
     Queue* queue;
     MessageProducer* producer;
     TextMessage* msg;
+    int num;
     AMQ_Worker() {};
 
 public:
     static void shutdown();
     
-    AMQ_Worker(Connection* connection, std::string& qName,
+    AMQ_Worker(Connection* connection, std::string& qName, int n,
         Session::AcknowledgeMode ackMode, int deliveryMode);
+    int getNum();
     virtual ~AMQ_Worker();
     void send(std::string& text);
     std::string getVersion();
@@ -74,10 +76,11 @@ extern "C" {
 
     const char *AMQ_GlobalInit(void);
     const char *AMQ_WorkerInit(AMQ_Worker **worker, AMQ_Connection *connection,
-        char *qName);
+                               char *qName, int num);
     const char *AMQ_Send(AMQ_Worker *worker, const char *data, unsigned len);
     const char *AMQ_Version(AMQ_Worker *worker, char *version);
     const char *AMQ_ClientID(AMQ_Worker *worker, char *clientID);
+    const char *AMQ_GetNum(AMQ_Worker *worker, int *num);
     const char *AMQ_WorkerShutdown(AMQ_Worker **worker);
     const char *AMQ_GlobalShutdown(void);
 
